059ccd0a57667bcefbb5c1e7a6994cde65c4389b
TobiDschi <39114750+TobiDschi@users.noreply.github.com>
2018-05-09 15:49:12 +0200

    Update data_analysis.R

--- data_analysis.R
+++ data_analysis.R
@@ -7,9 +7,10 @@
 
 library(glmmTMB)
 library(lsmeans)
+library(ggplot2)
 
-model1 = glmmTMB(tenasum ~ 1 + condition1 + condition2 + trial + (1|no/Pair), zi=~1, longdata, family=poisson)
-model2 = glmmTMB(tenasum ~ 1 + condition1 * condition2 + trial + (1|no/Pair), zi=~1, longdata, family=poisson)
+model1 <- glmmTMB(tenasum ~ 1 + condition1 + condition2 + trial + (1|no/Pair), zi=~1, longdata, family=poisson)
+model2 <- glmmTMB(tenasum ~ 1 + condition1 * condition2 + trial + (1|no/Pair), zi=~1, longdata, family=poisson)
 
 anova(model1, model2) # model comparison
 summary(model2) # model 2 was chosen to be relevant to the report
@@ -33,8 +34,23 @@ ggplot(longdata, aes(trial, tenasum, color = condition1)) +
   scale_color_brewer(name = "Competitiveness", palette = "Set2") 
 
 library(lme4)
-library(lmerTest)
+library(lsmeans)
 
 # H3
+
+model3 <- glmer(collapsedconcept ~ condition1 * condition2 + (1|no), longdata, ref = "politeness")
+summary(model3)
+
+# comparison of least square means - significance of condition differences
+
+lsmeans(model3, pairwise ~ condition1 * condition2, adjust = "tukey")
+
+# illustration 
+
+ggplot(longdata, aes(x = condition2, y = collapsedconcept_num, fill = condition1)) +
+	geom_bar(stat = "summary", fun.y = mean, position = "dodge") +
+	#geom_errorbar(stat = "summary", fun.data = mean_se, width = .2, position = position_dodge(width = 1)) +
+	labs(title = "Conceptualization of markers across conditions", x = "Functionality", y = "Conceptualization (proportion)") +
+	scale_fill_brewer(name = "Competitiveness", palette = "Set2")
 
 
