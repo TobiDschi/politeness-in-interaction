db3d63cb68dbc19277f576942c3dc484036276ee
TobiDschi <39114750+TobiDschi@users.noreply.github.com>
2018-05-09 14:40:32 +0200

    Create data_analysis.R

---
+++ data_analysis.R
@@ -0,0 +1,40 @@
+#data analysis
+
+#Here we will do a series of alternative data controlling for zero-inflation
+#see more at: https://cran.r-project.org/web/packages/glmmTMB/glmmTMB.pdf
+
+# H1 and H2
+
+library(glmmTMB)
+library(lsmeans)
+
+model1 = glmmTMB(tenasum ~ 1 + condition1 + condition2 + trial + (1|no/Pair), zi=~1, longdata, family=poisson)
+model2 = glmmTMB(tenasum ~ 1 + condition1 * condition2 + trial + (1|no/Pair), zi=~1, longdata, family=poisson)
+
+anova(model1, model2) # model comparison
+summary(model2) # model 2 was chosen to be relevant to the report
+
+lsmeans(model2, pairwise ~ condition1 * condition2, adjust = "tukey")
+
+# illustration
+
+ggplot(longdata, aes(condition2, tenasum, fill = condition1)) + 
+  geom_bar(stat = 'summary', fun.y = mean, position = "dodge") +
+  stat_summary(geom = 'errorbar', fun.data = mean_se, width = 0.2, position = position_dodge(width = 1)) +
+  labs(title = 'Politeness by functionality and competitiveness', x = 'Functionality', y = 'Politeness') +
+  scale_fill_brewer(name = "Competitiveness", palette = "Set2")
+
+# illustration of development over time
+
+ggplot(longdata, aes(trial, tenasum, color = condition1)) +
+  geom_smooth() +
+  facet_wrap(~ condition2) + 
+  labs(title = 'Politeness by conditions over time', x = 'Time (trial)', y = 'Politeness') + 
+  scale_color_brewer(name = "Competitiveness", palette = "Set2") 
+
+library(lme4)
+library(lmerTest)
+
+# H3
+
+
